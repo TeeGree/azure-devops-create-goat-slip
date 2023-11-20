ad3b3b8d54f78bf421048e43ce71947c313baf01
TeeGree <tsgriffi@gmail.com>
2023-11-19 19:14:34 -0500

    Add material ui, fix sample dialog.

---
+++ src/Samples/MyDialog/MyDialog.tsx
@@ -0,0 +1,71 @@
+import "./MyDialog.scss";
+
+import React, { useEffect, useState } from "react";
+import * as SDK from "azure-devops-extension-sdk";
+
+import { Button } from "@mui/material";
+import { ButtonGroup } from "azure-devops-ui/ButtonGroup";
+import { showRootComponent } from "../../Common";
+
+interface IPanelContentState {
+    message?: string;
+    toggleValue?: boolean;
+    ready?: boolean;
+}
+
+const MyDialog: React.FC = () => {
+    const [message, setMessage] = useState("");
+    useEffect(() => {
+        SDK.init();
+
+        SDK.ready().then(() => {
+            const config = SDK.getConfiguration();
+            const message = config.message || "Custom dialog message";
+            setMessage(message);
+
+            if (config.dialog) {
+                // Give the host frame the size of our dialog content so that the dialog can be sized appropriately.
+                // This is the case where we know our content size and can explicitly provide it to SDK.resize. If our
+                // size is dynamic, we have to make sure our frame is visible before calling SDK.resize() with no arguments.
+                // In that case, we would instead do something like this:
+                //
+                // SDK.notifyLoadSucceeded().then(() => {
+                // // we are visible in this callback.
+                // SDK.resize();
+                // });
+                SDK.resize(400, 400);
+            }
+        });
+    }, []);
+
+    const dismiss = () => {
+        const config = SDK.getConfiguration();
+        if (config.dialog) {
+            config.dialog.close();
+        } else if (config.panel) {
+            config.panel.close();
+        }
+    };
+
+    return (
+        <div className="sample-panel flex-column flex-grow">
+            {message}
+            <div
+                className="flex-grow flex-column flex-center justify-center"
+                style={{ border: "1px solid #eee", margin: "10px 0" }}
+            >
+                Sample time slip description
+            </div>
+            <ButtonGroup className="sample-panel-button-bar">
+                <Button variant="contained" onClick={() => dismiss()}>
+                    OK
+                </Button>
+                <Button variant="contained" onClick={() => dismiss()}>
+                    Close
+                </Button>
+            </ButtonGroup>
+        </div>
+    );
+};
+
+showRootComponent(<MyDialog />);
--- src/Samples/WorkItemMenu/WorkItemMenu.tsx
+++ src/Samples/WorkItemMenu/WorkItemMenu.tsx
@@ -1,26 +1,28 @@
 import "es6-promise/auto";
 import * as SDK from "azure-devops-extension-sdk";
 import {
-  CommonServiceIds,
-  IHostPageLayoutService,
+    CommonServiceIds,
+    IHostPageLayoutService,
 } from "azure-devops-extension-api";
 
 SDK.register("work-item-menu-action", () => {
-  return {
-    execute: async () => {
-      const dialogSvc = await SDK.getService<IHostPageLayoutService>(
-        CommonServiceIds.HostPageLayoutService
-      );
-      dialogSvc.openCustomDialog(
-        SDK.getExtensionContext().id + ".panel-content",
-        {
-          title: "Cool!",
-          lightDismiss: false,
-          onClose: () => console.log("Woo!"),
-        }
-      );
-    },
-  };
+    return {
+        execute: async () => {
+            const dialogSvc = await SDK.getService<IHostPageLayoutService>(
+                CommonServiceIds.HostPageLayoutService
+            );
+            dialogSvc.openCustomDialog(
+                SDK.getExtensionContext().id + ".my-dialog",
+                {
+                    title: "Enter time slip",
+                    configuration: {
+                        message: "Enter Time Slip Description:",
+                        dialog: true,
+                    },
+                }
+            );
+        },
+    };
 });
 
 SDK.init();
